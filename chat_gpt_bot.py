f55e5b272ff7f69b50adbebf8942e8f96a7de1c3
hadtheworld <65555179+hadtheworld@users.noreply.github.com>
2023-03-27 06:38:37 +0530

    Update chat_gpt_bot.py

--- chat_gpt_bot.py
+++ chat_gpt_bot.py
@@ -1,20 +1,19 @@
-# #!/usr/bin/env python3
-# #Import open AI OS and System Modules
-# import openai
-# openai.api_key = "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"
+#!/usr/bin/env python3
+#Import open AI OS and System Modules
+import openai
+openai.api_key = "[key-given]"
 
-# def chat(inp):
-#     response = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#             {"role": "system", "content": "You are a chatbot"},
-#             {"role": "user", "content": inp},
-#         ]
-#         )
-#     result = ''
-#     for choice in response.choices:
-#         result += choice.message.content
-#     return result
-# print(chat("who are you?"))
+def chat(inp):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": inp},
+        ]
+        )
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
+    return result
+print(chat("who are you?"))
 
-print("hello my friend")
